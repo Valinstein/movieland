38b17b2649ee91a33fe5ae28dfcedc453d56e027
Valinstein <valinstein@ukr.net>
2021-09-15 01:20:47 +0300

    added GetRandomMovies feature, without tests

--- src/main/java/com/valentyn/movieland/controller/MovieController.java
+++ src/main/java/com/valentyn/movieland/controller/MovieController.java
@@ -1,13 +1,15 @@
 package com.valentyn.movieland.controller;
 
+import com.valentyn.movieland.entity.Movie;
 import com.valentyn.movieland.service.MovieService;
-import com.valentyn.movieland.util.JsonMapper;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @Setter
@@ -17,8 +19,14 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping("/all")
-    public String findAll(HttpServletResponse resp) {
-        resp.setContentType("text/html;charset=utf-8");
-        return JsonMapper.toJson(movieService.getAllMovies());
+    public List<Movie> findAll(HttpServletResponse response) {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/random")
+    public List<Movie> getRandomMovies(@RequestParam (defaultValue = "3", name = "limit") String limit,
+                                       HttpServletResponse response){
+
+        return movieService.getRandomMovies(Integer.parseInt(limit));
     }
 }
--- src/test/java/com/valentyn/movieland/controller/MovieControllerTest.java
+++ src/test/java/com/valentyn/movieland/controller/MovieControllerTest.java
@@ -1,5 +1,6 @@
 package com.valentyn.movieland.controller;
 
+import com.jayway.jsonpath.JsonPath;
 import com.valentyn.movieland.service.MovieService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,8 +20,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static com.jayway.jsonassert.impl.matcher.IsCollectionWithSize.hasSize;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
@@ -40,7 +43,16 @@ class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("Check if response is in json format and that it contains a movie")
+    @DisplayName("Check if findAll() response in json format")
+    public void testIfAllMoviesIsJson() throws Exception {
+
+        RequestBuilder builder = MockMvcRequestBuilders.get("/movie/all");
+        MvcResult mvcResult = mockMvc.perform(builder).andExpect(ok)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+    }
+
+    @Test
+    @DisplayName("Check if findAll() contains a movie")
     public void testIfResponseIsJsonAndItIncludesMovie() throws Exception {
         String mockTestResult = "The Shawshank Redemption";
 
@@ -49,5 +61,24 @@ class MovieControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
         String res = mvcResult.getResponse().getContentAsString();
         assertTrue(res.contains(mockTestResult));
+    }
+
+    @Test
+    @DisplayName("Check if getRandomMovies() response is in json format")
+    public void testIfRandomMoviesAreJson() throws Exception {
+
+        RequestBuilder builder = MockMvcRequestBuilders.get("/movie/random");
+        MvcResult mvcResult = mockMvc.perform(builder).andExpect(ok)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+    }
+
+    @Test
+    @DisplayName("Check if getRandomMovies() contains a movie")
+    public void testIfRandomMoviesIncludesThreeMovies() throws Exception {
+
+        RequestBuilder builder = MockMvcRequestBuilders.get("/movie/all");
+        MvcResult mvcResult = (MvcResult) mockMvc.perform(builder)
+                .andExpect(jsonPath("$.*", hasSize(2)));
+        String res = mvcResult.getResponse().getContentAsString();
     }
 }
